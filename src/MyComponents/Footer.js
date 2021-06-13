12bb41a11986a0eea8dacdb3d3f073b9297226ca
Yogeshkumar04 <yogeshkumarbandhe@gmail.com>
2021-06-14 00:21:35 +0530

    ReactApp00

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+// import './footer.css'
+export const Footer = () => {
+
+  return (
+    <footer className="bg-dark text-light" >
+    <p className="text-center py-3">
+
+    Copyright &copy; TodoList.com
+    </p>
+      
+    </footer>
+  )
+}
+
+
+
+
+// style={footerStyles}
+// let footerStyles = {
+//   position: 'relative',
+//   top:'70vh',
+//   width: '100%'
+// }
